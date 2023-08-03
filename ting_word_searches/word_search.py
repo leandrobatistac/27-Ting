27167f67394a374134106992c94622551579656c
Leandro <leandrobcrv@gmail.com>
2023-08-03 09:35:33 -0300

    Requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    objeto = []
 
+    for arquivo in instance._data:
+        data = {
+            "palavra": word,
+            "arquivo": arquivo["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        linhas = arquivo.get("linhas_do_arquivo")
+        for i, ln in enumerate(linhas):
+            if word.lower() in ln.lower():
+                data["ocorrencias"].append({"linha": i+1})
+        if data["ocorrencias"]:
+            objeto.append(data)
+
+    return objeto
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
